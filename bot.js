3429287f9eabf93c12037e4e58011177bd53c928
GRP <50829508+headshot761@users.noreply.github.com>
2019-05-31 18:41:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,15 +43,15 @@ client.on("message", async message => {
                                 m.delete();
                                     message.channel.send(`:ballot_box_with_check: | Done ... The Broadcast Message Has Been Sent For ${message.guild.memberCount} Members`).then(msg => msg.delete(5000));
                                         message.guild.members.forEach(member => {
-                                            let bc = new Discord.RichEmbed()
-                                            .setColor("RANDOM")
-                                            .setThumbnail(message.author.avatarURL)
-                                            .setTitle("Broadcast")
-                                            .addField("Server", message.guild.name)
-                                            .addField("Sender", message.author.username)
-                                            .addField("Message", args);
+                                            let bc = new Discord.Message
+                                            
+                                            
+                                           
+                                            
+                                      
+                                             
 
-                                            member.sendEmbed(bc);
+                                           
                                         });
                         });
                         no.on("collect", v => {
